4d15d512339b78ae5bb1cd760d4ee98f180f255e
KagisoMonageng <63680048+KagisoMonageng@users.noreply.github.com>
2023-01-31 22:54:13 +0200

    login http direct

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,4 +1,4 @@
-import { HttpErrorResponse } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
@@ -23,7 +23,7 @@ export class LoginComponent implements OnInit {
     email: new FormControl()
   });
 
-  constructor(private router : Router,private toast : NgToastService,private authService: AuthService,private jwt : JwtService, private spinner : NgxSpinnerService) { }
+  constructor(private http : HttpClient,private router : Router,private toast : NgToastService,private authService: AuthService,private jwt : JwtService, private spinner : NgxSpinnerService) { }
 
   ngOnInit(): void {
     this.spinner.show()
@@ -41,7 +41,9 @@ export class LoginComponent implements OnInit {
   login(form : FormGroup)
   {
     this.spinner.show()
-    this.authService.login(form.value).subscribe( async (data: any)=>{
+    this.http.post('https://g-riend-gamer-app-e6y3ar47c-kagisomonageng.vercel.app/account/login',form.value).subscribe( async (data: any)=>{
+
+
       await this.authService.saveToken(data.token);
       sessionStorage.setItem('loggedIn_gamer',this.jwt.getData(data.token).gametag)
 
@@ -55,7 +57,7 @@ export class LoginComponent implements OnInit {
 
     },(error:HttpErrorResponse)=>{
       console.log(error)
-      this.toast.error({detail:"Sorry!", summary:error.error.message,position:'tr',duration:2000})
+      this.toast.error({detail:"Sorry!", summary:"Failed to login, Please retry",position:'tr',duration:2000})
       setTimeout(() => {
         this.spinner.hide();
 
