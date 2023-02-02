c684c7058034056dc61d46ce9d29ede941eb7c7b
KagisoMonageng <kagisoboasmonageng@gmail.com>
2023-02-02 14:27:27 +0200

    fixed error

--- backend/server.js
+++ backend/server.js
@@ -4,9 +4,11 @@ require("dotenv").config();
 const app = express();
 
 
-var corsOptions = {
+/* var corsOptions = {
   origin: "https://g-riend-gamer-app-tye2.vercel.app/",
-};
+  Access-Control-Request-Method: POST
+Access-Control-Request-Headers: Authorization, Content-Type
+}; */
 
 const account = require("./routes/account");
 const games = require("./routes/games");
@@ -14,7 +16,7 @@ const games = require("./routes/games");
 const port = 8080;
 
 app.use(express.json());
-app.use(cors(corsOptions));
+//app.use(cors(corsOptions));
 
 
 app.get('/', (req, res) => {
