a0cfccfe3c919ccb246dd9862ab6d4b5b12a7221
KagisoMonageng <kagisoboasmonageng@gmail.com>
2023-02-02 14:58:40 +0200

    fixed error

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,17 @@ const app = express();
   Access-Control-Request-Method: POST
 Access-Control-Request-Headers: Authorization, Content-Type
 }; */
+const allowCors = fn => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+}
 
 const account = require("./routes/account");
 const games = require("./routes/games");
@@ -16,7 +27,7 @@ const games = require("./routes/games");
 const port = 8080;
 
 app.use(express.json());
-//app.use(cors(corsOptions));
+app.use(cors(allowCors));
 
 
 app.get('/', (req, res) => {
