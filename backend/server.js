e7460fc3ef1eb932812a7d22a39f619e66fe8e2b
KagisoMonageng <63680048+KagisoMonageng@users.noreply.github.com>
2023-03-22 17:30:34 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,8 +14,8 @@ const games = require("./routes/games");
 const port = 8080;
 
 app.use(express.json());
-//app.use(cors({origin:'https://g-riend-gamer-app.vercel.app',methods:['GET', 'POST','PUT','DELETE','PATCH'],credentials:true}));
-app.use(cors({origin:'http://localhost:4200',methods:['GET', 'POST','PUT','DELETE','PATCH'],credentials:true}));
+app.use(cors({origin:'https://g-riend-gamer-app.vercel.app',methods:['GET', 'POST','PUT','DELETE','PATCH'],credentials:true}));
+//app.use(cors({origin:'http://localhost:4200',methods:['GET', 'POST','PUT','DELETE','PATCH'],credentials:true}));
 
 app.get('/',(req,res)=>{
   res.send('<div style="width: 100%; height:100vh; display:flex; flex-direction:column;gap:1rem ; justify-content:center; place-items:center;"> <div style="width:100px; height:100px; background-color: green; border-radius:50%"></div><h1 style="font-family:sans-serif;padding:0;margin:0;"> Server is running</h1> <p style="padding:0;margin:0;font-family:sans-serif;">Ready to take your requests</p></div>');
