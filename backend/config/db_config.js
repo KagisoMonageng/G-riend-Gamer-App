37d70112af129e4a830f811dfd4c9b118b0170bd
Kagiso Monageng <63680048+KagisoMonageng@users.noreply.github.com>
2023-06-06 20:33:45 +0200

    Update db_config.js

--- backend/config/db_config.js
+++ backend/config/db_config.js
@@ -1,5 +1,10 @@
-const pg = require('pg')
-const db = new pg.Client('postgres://oniuhcik:HVzf57D5a6DVllfAfv1UMAZBvL_0N9oa@hattie.db.elephantsql.com/oniuhcik');;
+// const pg = require('pg')
+// const db = new pg.Client('postgres://oniuhcik:HVzf57D5a6DVllfAfv1UMAZBvL_0N9oa@hattie.db.elephantsql.com/oniuhcik');;
+const { Pool } = require('pg')
+const db = new Pool({
+  connectionString: "postgres://default:Pz9YWI6VXgTO@ep-royal-morning-820086-pooler.us-east-1.postgres.vercel-storage.com:5432/verceldb?sslmode=require",
+})
+
 db.connect(function(err){
   if (err) {
     console.log("Database connection error");
